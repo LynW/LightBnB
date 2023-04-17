942c2f1cb21f34879298bb4d3b16a46dcc46591e
lynw <c54wang@gmail.com>
2023-04-17 19:28:07 -0400

    add more to test

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -18,7 +18,10 @@ VALUES ('Speed lamp', 'description', 123, 'https://images.pexels.com/photos/2086
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
 VALUES ('2018-09-11', '2018-09-26', 2, 3),
 ('2019-01-04', '2019-02-01', 2, 2),
-('2021-10-01', '2021-10-14', 1, 3);
+('2021-10-01', '2021-10-14', 1, 3),
+('2021-10-01', '2021-10-14', 1, 3),
+('2021-10-01', '2021-10-14', 1, 1001),
+('2021-10-01', '2021-10-14', 1, 1002);
 
 INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message) 
 VALUES ( 3, 2, 1, 3, 'messages'),

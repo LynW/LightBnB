be6fda9ea24bfb9949a54cf44ddaaeb5311bba09
lynw <c54wang@gmail.com>
2023-04-18 01:17:18 -0400

    fix seed errors

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -1,7 +1,7 @@
 INSERT INTO reservations (start_date, end_date, property_id, guest_id)
-VALUES ('2018-09-11', '2018-09-26', 1, 1),
+VALUES ('2018-09-11', '2018-09-26', 2, 3),
 ('2019-01-04', '2019-02-01', 2, 2),
-('2021-10-01', '2021-10-14', 3, 3);
+('2021-10-01', '2021-10-14', 1, 3);
 
 INSERT INTO users (name, email, password) 
 VALUES ('Eva Stanley', 'sebastianguerra@ymail.com', '$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
